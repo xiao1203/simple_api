f4ba283b7a0f009276f6ed46a3c4189710fd45a8
Xiao <x.xiao.turquoise@gmail.com>
2024-11-27 06:20:00 +0900

    flagAIP追加

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/sha1"
 	"encoding/hex"
+	"fmt"
 	"io"
 	"net/http"
 
@@ -15,6 +16,10 @@ type LoginRequest struct {
 	Password string `json:"password"`
 }
 
+type FlagRequest struct {
+	Flag string `json:"flag"`
+}
+
 // レスポンスボディの構造体
 type LoginResponse struct {
 	Token string `json:"token"`
@@ -25,6 +30,9 @@ func main() {
 
 	// ログインエンドポイントの設定
 	e.PUT("/login", login)
+
+	// フラグの受け取り口
+	e.PUT("/flag", flag)
 
 	// サーバーを起動
 	e.Logger.Fatal(e.Start(":8080"))
@@ -47,4 +55,20 @@ func login(c echo.Context) error {
 
 	// レスポンスを返す
 	return c.JSON(http.StatusOK, LoginResponse{Token: hex.EncodeToString(sha1.Sum(nil))})
+}
+
+func flag(c echo.Context) error {
+	req := new(FlagRequest)
+
+	// リクエストボディをバインド
+	if err := c.Bind(req); err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]string{"message": "invalid request"})
+	}
+
+	// フラグを返す
+	fmt.Println("-------------------------------")
+	fmt.Println("Flag: " + req.Flag)
+	fmt.Println("-------------------------------")
+
+	return c.String(http.StatusOK, "flag{this_is_fake_flag}")
 }
